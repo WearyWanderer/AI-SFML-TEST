7639e594c40b1f612ea31e0fe4cf13050e25d6fe
WearyWanderer <oglocisgangsta@hotmail.co.uk>
2015-02-24 22:30:23 +0000

    Title & FPS
    Changed window title, added a basic FPS counter that prints to console.
    This should just draw eventually to screen as text, and also be more
    accurate.

--- Main.cpp
+++ Main.cpp
@@ -7,26 +7,35 @@
 #pragma comment(lib, "gdi32.lib")  
 #endif // SFML_STATIC
 
-
+#include <iostream>
+#include <string>
 #include <SFML/Graphics.hpp>
 
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	sf::RenderWindow window(sf::VideoMode(960, 720), "AndrewAyre AI Pathfinding Algorithm Test");
+
+	sf::Clock clock;
+	float lastTime = 0;
+
 
 	while (window.isOpen())
 	{
+		#pragma region windowCloser
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
+		#pragma endregion
+
+		float currentTime = clock.restart().asSeconds();
+		float fps = 1.f / currentTime;
+		lastTime = currentTime;
+		std::cout << fps << std::endl; 
 
 		window.clear();
-		window.draw(shape);
 		window.display();
 	}
 
